7ca5f6e41d6ecca5357e19d6ec1d80ec3ff50a77
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-06-30 10:39:45 +0200

    feat(ipblock): allow to define blocked countries list or blocked ip in config

---
+++ lang/ipblock_fr.inc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+*
+* Fichier de traduction en francais de l'extension ipblock
+*
+*@package       ipblock
+*
+*/
+
+$GLOBALS['translations'] = array_merge(
+    $GLOBALS['translations'],
+    array(
+        'EDIT_CONFIG_HINT_ipblock_blocked_countries' => 'Tableau des pays bloqués (2 lettres par pays)',
+        'EDIT_CONFIG_HINT_ipblock_blocked_ips' => 'Tableau des ips bloquées',
+        )
+);
