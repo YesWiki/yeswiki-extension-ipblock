02ab7315d967e663473529e6941dff65e3537aaf
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-01-26 11:38:44 +0100

    doc(config): put example for editconfig

--- lang/ipblock_en.inc.php
+++ lang/ipblock_en.inc.php
@@ -10,7 +10,7 @@
 $GLOBALS['translations'] = array_merge(
     $GLOBALS['translations'],
     array(
-        'EDIT_CONFIG_HINT_ipblock_blocked_countries' => 'Array of blocked countries (2 letters by country)',
-        'EDIT_CONFIG_HINT_ipblock_blocked_ips' => 'Array of blocked ips',
+        'EDIT_CONFIG_HINT_ipblock_blocked_countries' => 'Array of blocked countries (2 letters by country ex: [\'ID\',\'MY\'])',
+        'EDIT_CONFIG_HINT_ipblock_blocked_ips' => 'Array of blocked ips (format [\'xxx.xxx.xxx.xxx\', \'xxx.xxx.xxx.xxx\'])',
         )
 );
--- lang/ipblock_fr.inc.php
+++ lang/ipblock_fr.inc.php
@@ -10,8 +10,8 @@
 $GLOBALS['translations'] = array_merge(
     $GLOBALS['translations'],
     array(
-        'EDIT_CONFIG_HINT_ipblock_blocked_countries' => 'Tableau des pays bloqués (2 lettres par pays)',
-        'EDIT_CONFIG_HINT_ipblock_blocked_ips' => 'Tableau des ips bloquées',
+        'EDIT_CONFIG_HINT_ipblock_blocked_countries' => 'Tableau des pays bloqués (2 lettres par pays ex: [\'ID\',\'MY\'])',
+        'EDIT_CONFIG_HINT_ipblock_blocked_ips' => 'Tableau des ips bloquées (format [\'xxx.xxx.xxx.xxx\', \'xxx.xxx.xxx.xxx\'])',
         'EDIT_CONFIG_GROUP_IPBLOCK' => 'Blocage d\'adresses IP',
         )
 );
